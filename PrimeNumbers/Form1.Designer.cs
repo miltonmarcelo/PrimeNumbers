3907e27d67f92a45fd95026d49a8bc237a48389b
Milton Marcelo <30089224+miltonmarcelo@users.noreply.github.com>
2024-06-18 00:12:58 +0100

    Add project files.

---
+++ PrimeNumbers/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PrimeNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(658, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Is this a Prime Number?";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(929, 237);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 31);
+            this.textBox.TabIndex = 1;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.outputLabel.Location = new System.Drawing.Point(693, 342);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(342, 124);
+            this.outputLabel.TabIndex = 2;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(608, 698);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(209, 77);
+            this.checkButton.TabIndex = 3;
+            this.checkButton.Text = "Check";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(929, 698);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(209, 77);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1914, 1224);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ PrimeNumbers/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private bool IsPrime (int number)
+        {
+            if (number <= 1) return false; //1 and 0 are not prime numbers
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number %i == 0) return false; //checking if the number is divisible by i
+            }
+            return true; //if no divisors found, it's prime
+        }
+
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox.Text, out int number))
+            {
+                if(IsPrime(number))
+                {
+                    outputLabel.Text = "The number " + number + "  is a prime number ";
+                }
+                else
+                {
+                    outputLabel.Text = "The number " + number + "  is not a prime number ";
+                }
+            }
+
+            else
+            {
+                outputLabel.Text = "Invalid input. Please enter an integer";
+            }
+        }
+    }
+}
